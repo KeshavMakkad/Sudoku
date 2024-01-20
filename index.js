05a3f05568655f8eef438769659454b6583b7a1e
KeshavMakkad <keshav.23bcs10002@ms.sst.scaler.com>
2024-01-21 01:52:46 +0530

    create new file for empty board generation and add number input function to the sudoku board

--- index.js
+++ index.js
@@ -1,37 +1,18 @@
 import newGame, { removeKEle } from "./js/gameLogic.js";
 import { NUM_ROWS } from "./js/constants.js";
+import { generateBoard } from "./js/generateGameBoard.js";
 
-const sudokuBoard = document.querySelector(".board");
-const numberBoard = document.querySelector(".numbers");
+generateBoard();
 
-const generateBoard = () => {
-  //Generate the tiles in the numbers which the player will select to add numbers in the main board
-  for (let i = 0; i < NUM_ROWS; i++) {
-    const numberTile = document.createElement("div"); // Generate the div in the numbers class
-    numberTile.setAttribute("id", i + 1); //set the id of the number. Used later.
-    numberTile.classList.add("tile"); // Add the class as tile to give styling
-    numberTile.classList.add("numbers-tile");
-    numberTile.innerText = i + 1; // Set the innertext as the umber
-    numberBoard.appendChild(numberTile); // Add it to the numbers board
-  }
+let active = 1;
 
-  //Generate the tiles in the main game board.
-  for (let i = 0; i < NUM_ROWS; i++) {
-    for (let j = 0; j < NUM_ROWS; j++) {
-      const gameTile = document.createElement("div"); // Create the div
-      const idOfGameTile = `${i + 1}-${j + 1}`; //Generate tje id of the tile
-      gameTile.setAttribute("id", idOfGameTile); // Set the id of the tile
-      gameTile.classList.add("tile"); // Add styling by using the tile class
-      gameTile.classList.add("game-board-tile");
-      sudokuBoard.appendChild(gameTile); //Append the div to the game board.....
-    }
-  }
-};
+let gameBoard;
 
 const createNewGame = () => {
-  const gameBoard = newGame();
-  const emptyGameBoard = removeKEle(40, gameBoard);
+  const CreateGameBoard = newGame();
+  const emptyGameBoard = removeKEle(40, CreateGameBoard);
 
+  gameBoard = JSON.parse(JSON.stringify(CreateGameBoard));
   for (let i = 0; i < NUM_ROWS; i++) {
     for (let j = 0; j < NUM_ROWS; j++) {
       const idOfGameTile = `${i + 1}-${j + 1}`;
@@ -39,19 +20,41 @@ const createNewGame = () => {
       tile.innerText = emptyGameBoard[i][j];
     }
   }
+
+  const active = document.getElementById("1");
+  active.classList.add("active");
 };
 
-let active = 1;
-
-const numberClicked = (selected) =>{
+const numberClicked = (selected) => {
   const activeNumber = document.getElementById(active);
   const selectedNumber = document.getElementById(selected);
   activeNumber.classList.remove("active");
   selectedNumber.classList.add("active");
   active = selected;
-}
+};
 
+const isValid = (row, colm) => {
+  return gameBoard[row][colm] == active ? true : false;
+};
 
+const wrongAns = (clickedTile) => {
+  return clickedTile.classList.contains("wrong-ans");
+};
+
+const gameTileClicked = (id) => {
+  const clickedTile = document.getElementById(id);
+  if (clickedTile.textContent !== "" && !wrongAns(clickedTile)) {
+    return;
+  }
+  const rowColmOfClicked = id.split("-");
+  if (
+    !isValid(rowColmOfClicked[0] - 1, rowColmOfClicked[1] - 1) &&
+    !wrongAns(clickedTile)
+  )
+    clickedTile.classList.add("wrong-ans");
+  else if (wrongAns(clickedTile)) clickedTile.classList.remove("wrong-ans");
+  clickedTile.textContent = active;
+};
 
 const addEventListeners = () => {
   const newGameBtn = document.querySelector("#new-game-btn");
@@ -60,13 +63,18 @@ const addEventListeners = () => {
   const numberTiles = document.querySelectorAll(".numbers-tile");
   for (let i = 0; i < numberTiles.length; i++) {
     numberTiles[i].addEventListener("click", () => {
-      numberClicked(i+1);
+      numberClicked(i + 1);
     });
   }
 
-  
+  const gameTiles = document.querySelectorAll(".game-board-tile");
+  // console.log(gameTiles)
+  for (let i = 0; i < gameTiles.length; i++) {
+    gameTiles[i].addEventListener("click", () => {
+      const getIdOfClicked = gameTiles[i].id;
+      gameTileClicked(getIdOfClicked);
+    });
+  }
 };
 
-generateBoard();
 createNewGame();
-addEventListeners();
---
+++ js/generateGameBoard.js
@@ -0,0 +1,37 @@
+// import { NUM_ROWS } from "./constants";
+
+const NUM_ROWS = 9;
+
+const sudokuBoard = document.querySelector(".board");
+const numberBoard = document.querySelector(".numbers");
+
+const generateNumberBoard = () => {
+  //Generate the tiles in the numbers which the player will select to add numbers in the main board
+  for (let i = 0; i < NUM_ROWS; i++) {
+    const numberTile = document.createElement("div"); // Generate the div in the numbers class
+    numberTile.setAttribute("id", i + 1); //set the id of the number. Used later.
+    numberTile.classList.add("tile"); // Add the class as tile to give styling
+    numberTile.classList.add("numbers-tile");
+    numberTile.innerText = i + 1; // Set the innertext as the umber
+    numberBoard.appendChild(numberTile); // Add it to the numbers board
+  }
+};
+
+const generateGameBoard = () => {
+  //Generate the tiles in the main game board.
+  for (let i = 0; i < NUM_ROWS; i++) {
+    for (let j = 0; j < NUM_ROWS; j++) {
+      const gameTile = document.createElement("div"); // Create the div
+      const idOfGameTile = `${i + 1}-${j + 1}`; //Generate tje id of the tile
+      gameTile.setAttribute("id", idOfGameTile); // Set the id of the tile
+      gameTile.classList.add("tile"); // Add styling by using the tile class
+      gameTile.classList.add("game-board-tile");
+      sudokuBoard.appendChild(gameTile); //Append the div to the game board.....
+    }
+  }
+};
+
+export function generateBoard(){
+    generateNumberBoard();
+    generateGameBoard();
+}
